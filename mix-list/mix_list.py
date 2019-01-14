b1a6a433c90f1343e7ada6f2b8a921907ca1b24b
Brandon Bazan <bbazan@vmware.com>
2019-01-14 17:09:03 -0500

    testing printing from lists in python

---
+++ mix-list/mix_list.py
@@ -0,0 +1,12 @@
+#!/usr/biin/env python3
+my_list = [ "192.168.0.5", "5060", "UP" ]
+print("The First item in the list (IP): " + my_list[0] )
+print("The second item in the list (port): " + my_list[1] )
+print("The last item in the list (state): " + my_list[2] )
+
+#This will print the different items in the list by position
+
+
+new_list = [ "5060", "80", "55", "10.0.0.1", "10.20.30.1", "ssh" ]
+
+print("This will print a list of numbers and a few words, such as IPs: " + new_list[3] + " and " + new_list[4] +", ports " + new_list[0] +", " + new_list[1] + ", and " + new_list[2] + " and a single word: " + new_list[5] +". " )
